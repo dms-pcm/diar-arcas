24a748ddd55f263d6664df774007bbd67e78f2fa
dms-pcm <81421412+dms-pcm@users.noreply.github.com>
2022-11-03 16:25:19 +0700

    update login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('assets/css/mystyle.css')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <title>Document</title>
 </head>
 <body>
@@ -25,16 +26,28 @@
             </div>
             <form class="p-3 mt-3">
                 <div class="form-field d-flex align-items-center">
-                    <span class="far fa-user"></span>
+                    <span class="far fa-user ml-2"></span>
                     <input type="text" name="userName" id="userName" placeholder="Username">
                 </div>
                 <div class="form-field d-flex align-items-center">
-                    <span class="fas fa-key"></span>
+                    <span class="fas fa-key ml-2"></span>
                     <input type="password" name="password" id="pwd" placeholder="Password">
                 </div>
                 <button class="btn mt-3">Login</button>
             </form>
         </div>
+        <ul class="bg-bubbles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
     </div>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
