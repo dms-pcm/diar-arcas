df04834daf28609df1fa4a69ec959fe830acb9b2
aryawb <61925684+aryawb@users.noreply.github.com>
2022-11-07 14:09:55 +0700

    pasang css dan halaman admin

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,209 @@
+@extends('layout.app') @section('content')
+
+<div class="app-content content">
+  <div class="content-wrapper">
+    <div class="content-header row"></div>
+    <div class="content-body">
+      <div class="row">
+        <div class="col-xl-4 col-lg-12 col-md-12">
+          <div class="card">
+            <div class="card-content">
+              <div class="card-body">
+                <div class="media">
+                  <div class="media-body text-left">
+                    <h1 class="success">80</h1>
+                    <span>Total Pengajuan Izin Lainnya</span>
+                  </div>
+                  <div class="media-right media-middle">
+                    <i class="ft-award success font-large-2 float-right"></i>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-4 col-lg-12 col-md-12">
+          <div class="card">
+            <div class="card-content">
+              <div class="card-body">
+                <div class="media">
+                  <div class="media-body text-left">
+                    <h1 class="deep-orange">70</h1>
+                    <span>Total Pengajuan Izin Sakit</span>
+                  </div>
+                  <div class="media-right media-middle">
+                    <i class="ft-package deep-orange font-large-2 float-right"></i>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-4 col-lg-12 col-md-12">
+          <div class="card">
+            <div class="card-content">
+              <div class="card-body">
+                <div class="media">
+                  <div class="media-body text-left">
+                    <h1 class="info">6</h1>
+                    <span>Total Pengajuan Cuti</span>
+                  </div>
+                  <div class="media-right media-middle">
+                    <i class="ft-users info font-large-2 float-right"></i>
+                  </div>
+                </div>
+               
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12">
+          <div class="card">
+            <div class="card-body">
+              <ul class="list-inline text-center mt-3">
+                <li>
+                  <h6>
+                    <i class="ft-circle danger"></i> Page Views
+                  </h6>
+                </li>
+                <li>
+                  <h6>
+                    <i class="ft-circle success pl-1"></i> Total Visit
+                  </h6>
+                </li>
+                <li>
+                  <h6>
+                    <i class="ft-circle warning pl-1"></i> Unique Visitor
+                  </h6>
+                </li>
+              </ul>
+              <div class="chartjs">
+                <canvas id="visitors-graph" height="275"></canvas>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Daftar Kehadiran Karyawan Per Hari</h3>
+              <a class="heading-elements-toggle">
+                <i class="fa fa-ellipsis-v font-medium-3"></i>
+              </a>
+              <div class="heading-elements">
+                <ul class="list-inline mb-0">
+                  <li>
+                    <a data-action="collapse">
+                      <i class="ft-minus"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a data-action="reload">
+                      <i class="ft-rotate-cw"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a data-action="expand">
+                      <i class="ft-maximize"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="card-content collapse show">
+            <div class="card-body">
+              
+              <table class="table table-striped table-bordered zero-configuration">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Position</th>
+                    <th>Office</th>
+                    <th>Age</th>
+                    <th>Start date</th>
+                    <th>Salary</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Tiger Nixon</td>
+                    <td>System Architect</td>
+                    <td>Edinburgh</td>
+                    <td>61</td>
+                    <td>2011/04/25</td>
+                    <td>$320,800</td>
+                  </tr>
+                  <tr>
+                    <td>Garrett Winters</td>
+                    <td>Accountant</td>
+                    <td>Tokyo</td>
+                    <td>63</td>
+                    <td>2011/07/25</td>
+                    <td>$170,750</td>
+                  </tr>
+                  <tr>
+                    <td>Ashton Cox</td>
+                    <td>Junior Technical Author</td>
+                    <td>San Francisco</td>
+                    <td>66</td>
+                    <td>2009/01/12</td>
+                    <td>$86,000</td>
+                  </tr>
+                  <tr>
+                    <td>Cedric Kelly</td>
+                    <td>Senior Javascript Developer</td>
+                    <td>Edinburgh</td>
+                    <td>22</td>
+                    <td>2012/03/29</td>
+                    <td>$433,060</td>
+                  </tr>
+                  <tr>
+                    <td>Airi Satou</td>
+                    <td>Accountant</td>
+                    <td>Tokyo</td>
+                    <td>33</td>
+                    <td>2008/11/28</td>
+                    <td>$162,700</td>
+                  </tr>
+                  <tr>
+                    <td>Brielle Williamson</td>
+                    <td>Integration Specialist</td>
+                    <td>New York</td>
+                    <td>61</td>
+                    <td>2012/12/02</td>
+                    <td>$372,000</td>
+                  </tr>
+                  <tr>
+                    <td>Herrod Chandler</td>
+                    <td>Sales Assistant</td>
+                    <td>San Francisco</td>
+                    <td>59</td>
+                    <td>2012/08/06</td>
+                    <td>$137,500</td>
+                  </tr>
+                  <tr>
+                    <td>Rhona Davidson</td>
+                    <td>Integration Specialist</td>
+                    <td>Tokyo</td>
+                    <td>55</td>
+                    <td>2010/10/14</td>
+                    <td>$327,900</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- ////////////////////////////////////////////////////////////////////////////-->
+ @endsection
---
+++ resources/views/lemburadmin.blade.php
@@ -0,0 +1,365 @@
+@extends('layout.app') @section('content')
+
+<div class="app-content content">
+	<div class="content-wrapper">
+		<div class="content-header row"></div>
+		<div class="content-body">
+
+			<div class="row">
+				<div class="col-xl-12 col-lg-12 col-md-12">
+					<div class="card">
+						<div class="card-header d-flex align-items-center">
+							<div class="d-flex align-items-center">
+								<h3 class="card-title mr-2">Daftar Lembur Karyawan</h3>
+								<a href="#" class="btn btn-blue btn-sm" title="" data-toggle="modal" data-target="#tambah">Buat Data Lembur</a>
+							</div>
+							<a class="heading-elements-toggle">
+								<i class="fa fa-ellipsis-v font-medium-3"></i>
+							</a>
+							<div class="heading-elements">
+								<ul class="list-inline mb-0">
+									<li>
+										<a data-action="collapse">
+											<i class="ft-minus"></i>
+										</a>
+									</li>
+									<li>
+										<a data-action="reload">
+											<i class="ft-rotate-cw"></i>
+										</a>
+									</li>
+									<li>
+										<a data-action="expand">
+											<i class="ft-maximize"></i>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div class="card-content collapse show">
+							<div class="card-body">
+
+								<table class="table table-striped table-bordered zero-configuration">
+									<thead>
+										<tr>
+											<th>No</th>
+											<th>Nama Karyawan</th>
+											<th>Tgl Lembur</th>
+											<th>Lama Lembur</th>
+											<th>Selesai</th>
+											<th>Aksi</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>1</td>
+											<td>Tiger Nixon</td>
+											<td></td>
+											<td></td>
+											<td></td>
+											<td>
+												<a href="#" title="" class="btn btn-sm btn-cyan text-white" data-toggle="modal" data-target="#view">
+													<i class="fa fa-eye" aria-hidden="true"></i>
+												</a>
+													<!-- <a href="#" title="" class="btn btn-sm btn-success text-white" data-toggle="modal" data-target="#edit">
+														<i class="fa fa-edit" aria-hidden="true"></i>
+													</a>
+													<a href="#" title="" class="btn btn-sm btn-danger text-white" id="hapus-data">
+														<i class="fa fa-trash" aria-hidden="true"></i>
+													</a> -->
+
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-xl-12 col-lg-12 col-md-12">
+						<div class="card">
+							<div class="card-header d-flex align-items-center">
+								<div class="d-flex align-items-center">
+									<h3 class="card-title mr-2">Pengajuan Cuti Lembur Karyawan</h3>
+
+								</div>
+								<a class="heading-elements-toggle">
+									<i class="fa fa-ellipsis-v font-medium-3"></i>
+								</a>
+								<div class="heading-elements">
+									<ul class="list-inline mb-0">
+										<li>
+											<a data-action="collapse">
+												<i class="ft-minus"></i>
+											</a>
+										</li>
+										<li>
+											<a data-action="reload">
+												<i class="ft-rotate-cw"></i>
+											</a>
+										</li>
+										<li>
+											<a data-action="expand">
+												<i class="ft-maximize"></i>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+							<div class="card-content collapse show">
+								<div class="card-body">
+
+									<table class="table table-striped table-bordered zero-configuration">
+										<thead>
+											<tr>
+												<th>No</th>
+												<th>Nama Karyawan</th>
+												<th>Tgl Lembur</th>
+												<th>Lama Lembur</th>
+												<th>Status</th>
+												<th>Aksi</th>
+
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>1</td>
+												<td>Tiger Nixon</td>
+												<td></td>
+												<td></td>												<td>
+													<p class="badge badge-success round">Disetujui</p>
+													<p class="badge badge-danger round">Ditolak</p>
+													<p class="badge badge-warning round">Menunggu</p>
+												</td>
+												<td>
+													<a href="#" title="" class="btn btn-sm btn-cyan text-white" data-toggle="modal" data-target="#viewpengajuan">
+														<i class="fa fa-eye" aria-hidden="true"></i>
+													</a>
+													<!-- <div class="d-flex">
+														<a href="#" title="" class="btn btn-sm btn-success text-white mr-1" data-toggle="modal" data-target="#edit" id="setuju-lembur">
+															<i class="fa fa-check" aria-hidden="true"></i>
+														</a>
+														<a href="#" title="" class="btn btn-sm btn-danger text-white" id="hapus-data" id="tolak-lembur">
+															<i class="fa fa-times" aria-hidden="true"></i>
+														</a>
+													</div> -->
+												</td>
+
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Modal view -->
+	<div class="modal animated zoomIn text-left" id="view" tabindex="-1" role="dialog" aria-labelledby="myModalLabel69" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title" id="myModalLabel69">Detail Data Lembur Karyawan</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<div class="card bg-grey bg-lighten-4">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-xl-4 col-lg-12 col-md-12">
+									<p>Nama Karyawan</p>
+									<p>Jabatan</p>
+									<p>Tanggal lembur</p>
+									<p>Lama Lembur</p>
+									<p>Selesai</p>
+									<p>Deskripsi</p>
+								</div>
+								<div class="col-xl-8 col-lg-12 col-md-12">
+									<p>:&nbsp; Handoko</p>
+									<p>:&nbsp; Handoko</p>
+									<p>:&nbsp; Handoko</p>
+									<p>:&nbsp; Handoko</p>
+									<p>:&nbsp; Handoko</p>
+									<p>:&nbsp; Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn grey btn-outline-secondary" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Modal view-->
+
+	<!-- Modal view pengajuan -->
+	<div class="modal animated zoomIn text-left" id="viewpengajuan" tabindex="-1" role="dialog" aria-labelledby="myModalLabel69" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title" id="myModalLabel69">Detail Persetujuan Lembur Karyawan</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<div class="card bg-grey bg-lighten-4">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-xl-4 col-lg-12 col-md-12">
+									<p>Nama Karyawan</p>
+									<p>Jabatan</p>
+									<p>Tanggal lembur</p>
+									<p>Lama Lembur</p>
+									<p>Selesai</p>
+									<p>Status</p>
+									<p>Deskripsi</p>
+								</div>
+								<div class="col-xl-8 col-lg-12 col-md-12">
+									<p>:&nbsp; Handoko</p>
+									<p>:&nbsp; Handoko</p>
+									<p>:&nbsp; Handoko</p>
+									<p>:&nbsp; Handoko</p>
+									<p>:&nbsp; Handoko</p>
+									<p>:&nbsp; 
+										<span class="badge badge-success round">Disetujui</span>
+										<span class="badge badge-danger round">Ditolak</span>
+										<span class="badge badge-warning round">Menunggu</span>
+									</p>
+									<p>:&nbsp; Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.</p>
+								</div>
+							</div>
+							<div class="row justify-content-center	">
+								
+								<a href="#" title="" class="btn btn-danger mr-1 text-white" id="tolak-lembur">
+									<span>Tolak <i class="fa fa-times" aria-hidden="true"></i></span>
+								</a>
+								<a href="#" title="" class="btn btn-success text-white mr-1" data-toggle="modal" data-target="#edit" id="setuju-lembur">
+									<span>Setuju <i class="fa fa-check" aria-hidden="true"></i></span>
+								</a>
+
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn grey btn-outline-secondary" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Modal view pengajuan-->
+
+
+	<!-- Modal tambah -->
+	<div class="modal animated zoomIn text-left" id="tambah" tabindex="-1" role="dialog" aria-labelledby="myModalLabel69" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title" id="myModalLabel69">Tambah Data Lembur Karyawan</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<form class="form">
+						<div class="form-body">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="form-group">
+										<label for="projectinput1">Nama Karyawan</label>
+										<div class="wrapper">
+											<select name="" id="" class="custom-select" onfocus='this.size=5;' onblur='this.size=1;' onchange='this.size=1; this.blur();'>
+												<option value="">One</option>
+												<option value="">Two</option>
+												<option value="">Three</option>
+												<option value="">Four</option>
+												<option value="">Five</option>
+												<option value="">Six</option>
+												<option value="">Seven</option>
+												<option value="">Eight</option>
+												<option value="">Nine</option>
+												<option value="">Ten</option>
+											</select></div>
+										</div>
+									</div>
+
+								</div>
+								<div class="row">
+									<div class="col-md-6">
+										<div class="form-group">
+											<label for="projectinput1">Jabatan Karyawan</label>
+											<div class="wrapper">
+												<select name="" id="" class="custom-select" onfocus='this.size=5;' onblur='this.size=1;' onchange='this.size=1; this.blur();'>
+													<option value="">One</option>
+													<option value="">Two</option>
+													<option value="">Three</option>
+													<option value="">Four</option>
+													<option value="">Five</option>
+													<option value="">Six</option>
+													<option value="">Seven</option>
+													<option value="">Eight</option>
+													<option value="">Nine</option>
+													<option value="">Ten</option>
+												</select></div>
+											</div>
+										</div>
+
+										<div class="col-md-6">
+											<div class="form-group">
+												<label for="projectinput3">Tanggal Lembur</label>
+												<input type="text" class="form-control" id="animate" placeholder="Tanggal Lembur">
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-md-6">
+											<div class="form-group">
+												<label for="projectinput4">Lama Lembur</label>
+												<input type="text" id="projectinput4" class="form-control" placeholder="1 Jam" name="phone">
+											</div>
+										</div>
+										<div class="col-md-6">
+											<div class="form-group">
+												<label for="projectinput4">Selesai</label>
+												<input type="text" class="form-control" id="time_init_animation" placeholder="Selesai Lembur">
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-md-12">
+											<div class="form-group">
+												<label for="">Deskripsi</label>
+												input
+												<textarea name="" class="form-control"></textarea>
+											</div>
+										</div>
+									</div>
+								</div>
+							</form>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn grey btn-outline-secondary" data-dismiss="modal">Close</button>
+							<button type="button" class="btn btn-outline-success">Save changes</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Modal tambah-->
+			<script src="{{asset('assets/vendors/js/extensions/sweetalert.min.js')}}"></script>
+			<!-- sweetalert ada disini -->
+			<script src="{{asset('assets/js/scripts/extensions/sweet-alerts.min.js')}}"></script>
+			<!-- ////////////////////////////////////////////////////////////////////////////-->
+			@endsection
